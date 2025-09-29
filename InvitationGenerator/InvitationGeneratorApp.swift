ac817e25b2959494fac019bd0a9fca1a9cc348b8
Anthony Tran <qquang269@gmail.com>
2025-09-29 10:24:59 +0700

    Initial Commit

---
+++ InvitationGenerator/InvitationGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvitationGeneratorApp.swift
+//  InvitationGenerator
+//
+//  Created by Anthony on 29/9/25.
+//
+
+import SwiftUI
+
+@main
+struct InvitationGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InvitationGeneratorTests/InvitationGeneratorTests.swift
@@ -0,0 +1,17 @@
+//
+//  InvitationGeneratorTests.swift
+//  InvitationGeneratorTests
+//
+//  Created by Anthony on 29/9/25.
+//
+
+import Testing
+@testable import InvitationGenerator
+
+struct InvitationGeneratorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
